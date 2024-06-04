2e7438e5ac6f5246fa6a1f444da0ec0556bc42a0
Tadeu Paiva <tadeupaiva11@gmail.com>
2024-06-03 22:25:35 -0300

    Implementando Sessions na aplicacao

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
-const usersRoutes = require("./users.routes")
-const notesRouter = require("./notes.routes")
-const tagsRouter = require("./tags.routes")
+const usersRoutes = require("./users.routes");
+const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
+const sessionRoutes = require("./sessions.routes");
 
-const routes = Router()
+const routes = Router();
 
-routes.use("/users", usersRoutes)
-routes.use("/notes", notesRouter)
-routes.use("/tags", tagsRouter)
+routes.use("/users", usersRoutes);
+routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
+routes.use("/sessions", sessionRoutes);
 
-module.exports = routes
+module.exports = routes;
